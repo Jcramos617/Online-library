5a9ce2bcc6a1abf9a4485c854c131f1977d9e371
Jordan Ramos <56659229+Jcramos617@users.noreply.github.com>
2019-12-11 18:29:36 -0500

    Add files via upload

---
+++ Online-library/inc/data.php
@@ -0,0 +1,167 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Animal Farm",
+	"img" => "img/media/AnimalFarm.jpg",
+    "genre" => "Politcal satire",
+    "format" => "Hardcover",
+    "year" => 1945,
+    "category" => "Books",
+    "authors" => [
+        "George Orwell"
+    ],
+    "publisher" => "Harcourt, Brace and Company",
+    "isbn" => '9789630793292'
+];
+$catalog[102] = [
+    "title" => "Preacher",
+    "img" => "img/media/Preacher.jpg",
+    "genre" => "Supernatural",
+    "format" => "Paperback",
+    "year" => 1995,
+    "category" => "Books",
+    "authors" => [
+        "Garth Ennis, Steve Dillon"
+    ],
+    "publisher" => "Vertigo",
+    "isbn" => '9781401240455'
+];
+$catalog[103] = [
+    "title" => "Where We Go from Here: Two Years in the Resistance",
+    "img" => "img/media/Bernie.jpg",
+    "genre" => "non-fiction",
+    "format" => "Hardcover",
+    "year" => 2018,
+    "category" => "Books",
+    "authors" => [
+        "Bernie Sanders"
+    ],
+    "publisher" => "Thomas Dunne Books; 1st Edition edition",
+    "isbn" => '1250163269'
+];
+$catalog[104] = [
+    "title" => "Watchmen",
+    "img" => "img/media/Watchmen.jpg",
+    "genre" => "Science fiction",
+    "format" => "Paperback",
+    "year" => 1986,
+    "category" => "Books",
+    "authors" => [
+        "Alan Moore, Dave Gibbons"
+    ],
+    "publisher" => "DC Comics",
+    "isbn" => '1401245250'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Spider Man Into the Spider Verse",
+    "img" => "img/media/Spiderman.jpg",
+    "genre" => "Animation",
+    "format" => "Streaming",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "Peter Ramsey, Bob Persichetti, Rodney Rothman",
+    "writers" => [
+        "Phil Lord",
+        "Rodney Rothman"
+    ],
+    "stars" => [
+        "Shameik Moore",
+        "Jake Johnson",
+        "Hailee Steinfeld",
+    ]
+];
+$catalog[202] = [
+    "title" => "Avengers: Endgame",
+    "img" => "img/media/Avengers.jpg",
+    "genre" => "Science Fiction",
+    "format" => "Streaming",
+    "year" => 2019,
+    "category" => "Movies",
+    "director" => "Anthony Russo, Joe Russo",
+    "writers" => [
+        "Stephen McFeely",
+				"Christopher Markus"
+    ],
+    "stars" => [
+			"Robert Downey Jr",
+			"Chris Evans",
+			"Chris Hemsworth"
+    ]
+];
+$catalog[203] = [
+    "title" => "The Big Lebowski",
+    "img" => "img/media/BigLebowski.jpg",
+    "genre" => "Dark comedy",
+    "format" => "DVD/streaming",
+    "year" => 1998,
+    "category" => "Movies",
+    "director" => "Joel Coen, Ethan Coen ",
+    "writers" => [
+        "Joel Coen",
+        "Ethan Coen"
+    ],
+    "stars" => [
+			"Jeff Bridges",
+			"John Goodman",
+			"Julianne Moore"
+    ]
+];
+$catalog[204] = [
+    "title" => "Mad Max: Fury Road",
+		"img" => "img/media/MadMax.jpg",
+	  "genre" => "Science Fiction, Adventure",
+	  "format" => "DVD/Streaming",
+	  "year" => 2015,
+	  "category" => "Movies",
+	  "director" => "George Miller",
+	  "writers" => [
+	      "George Miller"
+
+	  ],
+	  "stars" => [
+	      "Tom Hardy",
+	      "Charlize Theron",
+	      "Nicholas Hoult"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "Beatles Let It Be",
+	"img" => "img/media/Beatles.jpg",
+	"genre" => "Rock",
+	"format" => "CD/streaming",
+	"year" => 1970,
+	"category" => "Music",
+	"artist" => "The Beatles"
+];
+
+$catalog[302] = [
+	"title" => "Machine Head",
+	"img" => "img/media/DeepPurple.jpg",
+	"genre" => "Early heavy metal",
+	"format" => "Streaming/CD",
+	"year" => 1971,
+	"category" => "Music",
+	"artist" => "Deep Purple"
+];
+$catalog[303] = [
+	"title" => "Beggers Banquet",
+	"img" => "img/media/RollingStones.jpg",
+	"genre" => "Rock",
+	"format" => "CD, Streaming Services",
+	"year" => 1968,
+	"category" => "Music",
+	"artist" => "Rolling Stones"
+];
+$catalog[304] = [
+	"title" => "Wasteland",
+	"img" => "img/media/UncleAcid.jpg",
+	"genre" => "Stoner Rock",
+	"format" => "Streaming Services",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "Uncle Acid & the Deadbeats"
+];
+?>
